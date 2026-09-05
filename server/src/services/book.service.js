fd592623fb01f12f00c48af03977f8c481e23998
mohsinking2002 <mohsinraja4667@gmail.com>
2026-09-05 20:20:17 +0530

    add custom error handler utility

--- server/src/services/book.service.js
+++ server/src/services/book.service.js
@@ -5,6 +5,7 @@ import {
   updateBookDetails,
   deleteBookByID,
 } from "../repositories/book.repository.js";
+import { AppError } from "../utils/errors.js";
 
 export const getBooks = () => {
   return getAllBooks();
@@ -16,10 +17,12 @@ export const getBook = (id) => {
 
 export const createBook = (input) => {
   //error handling
-  if (!input.title.trim()) throw new Error("Book title is required");
-  if (!input.author.trim()) throw new Error("Book author is required");
+  if (!input.title.trim())
+    throw new AppError("Book title is required", "VALIDATION_ERROR");
+  if (!input.author.trim())
+    throw new AppError("Book author is required", "VALIDATION_ERROR");
   if (input.publishedYear !== undefined && input.publishedYear < 0)
-    throw new Error("Invalid publish year");
+    throw new AppError("Invalid publish year", "VALIDATION_ERROR");
 
   // create book object with id
   const book = {
@@ -32,21 +35,21 @@ export const createBook = (input) => {
 
 export const updateBook = (id, input) => {
   const existingBook = getBookById(id);
-  if (!existingBook) throw new Error("Book not found");
+  if (!existingBook) throw new AppError("Book not found", "BOOK_NOT_FOUND");
 
   if (input.title !== undefined && !input.title.trim())
-    throw new Error("Book title cannot be Empty");
+    throw new AppError("Book title cannot be Empty", "VALIDATION_ERROR");
   if (input.author !== undefined && !input.author.trim())
-    throw new Error("Book author cannot be Empty");
+    throw new AppError("Book author cannot be Empty", "VALIDATION_ERROR");
   if (input.publishedYear !== undefined && input.publishedYear < 0)
-    throw new Error("Invalid publised year");
+    throw new AppError("Invalid publised year", "VALIDATION_ERROR");
 
   return updateBookDetails(id, input);
 };
 
 export const deleteBook = (id) => {
   const existingBook = getBookById(id);
-  if (!existingBook) throw new Error("Book not found");
+  if (!existingBook) throw new AppError("Book not found");
 
   return deleteBookByID(id);
 };
