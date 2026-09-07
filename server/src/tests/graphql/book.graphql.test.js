ffdf42d831955b6a45d37659535ae8d83c5cfafa
mohsinking2002 <mohsinraja4667@gmail.com>
2026-09-07 10:37:14 +0530

    add integration tests for graphql-queries

--- server/src/tests/graphql/book.graphql.test.js
+++ server/src/tests/graphql/book.graphql.test.js
@@ -1,9 +1,10 @@
 import { describe, expect, it } from "vitest";
-import { server } from "../../app.js";
+import { createTestServer } from "../test-server.js";
 import { executeOperation } from "../helpers/graphql-test-client.js";
 
 describe("Book GraphQL API", () => {
   it("it should fetch all books", async () => {
+    const server = createTestServer();
     const query = `
         query {
             books {
@@ -16,8 +17,87 @@ describe("Book GraphQL API", () => {
     `;
 
     const response = await executeOperation(server, query);
+
     expect(response.body.kind).toBe("single");
     expect(response.body.singleResult.error).toBeUndefined();
-    expect(response.body.singleResult.data.books).toBeDefined();
+    expect(response.body.singleResult.data.books).toHaveLength(2);
+    expect(response.body.singleResult.data.books[0]).toEqual({
+      id: "1",
+      title: "Test Book 1",
+      author: "Test Author 1",
+      publishedYear: 2020,
+    });
+  });
+
+  it("should fetch a book by id", async () => {
+    const server = createTestServer();
+
+    const query = `
+      query {
+        book(id: "1") {
+          id
+          title
+          author
+          publishedYear
+        }
+      }
+    `;
+
+    const response = await executeOperation(server, query);
+
+    expect(response.body.singleResult.errors).toBeUndefined();
+    expect(response.body.singleResult.data.book).toEqual({
+      id: "1",
+      title: "Test Book 1",
+      author: "Test Author 1",
+      publishedYear: 2020,
+    });
+  });
+
+  it("should return null when book doesn't exist", async () => {
+    const server = createTestServer();
+
+    const query = `
+      query {
+        book(id: "id-doesn't-exist") {
+          id
+          title
+          author
+          publishedYear
+        }
+      }
+    `;
+
+    const response = await executeOperation(server, query);
+
+    expect(response.body.singleResult.errors).toBeUndefined();
+    expect(response.body.singleResult.data.book).toBeNull();
+  });
+
+  it("should fetch a book using GraphQL variables", async () => {
+    const server = createTestServer();
+
+    const query = `
+      query GetBook($id: ID!) {
+        book(id: $id) {
+          id
+          title
+          author
+          publishedYear
+        }
+      }
+    `;
+
+    const response = await executeOperation(server, query, {
+      id: "2",
+    });
+
+    expect(response.body.singleResult.errors).toBeUndefined();
+    expect(response.body.singleResult.data.book).toEqual({
+      id: "2",
+      title: "Test Book 2",
+      author: "Test Author 2",
+      publishedYear: 2021,
+    });
   });
 });
---
+++ server/src/tests/test-server.js
@@ -0,0 +1,18 @@
+import { ApolloServer } from "@apollo/server";
+import { typeDefs } from "../graphql/schema/index";
+import { createBookResolvers } from "../graphql/resolvers/book.resolver";
+import { createBookService } from "../services/book.service";
+import { createBookRepository } from "../repositories/book.repository";
+import { testBooks } from "./fixtures/book.fixture.js";
+
+export const createTestServer = () => {
+  const books = structuredClone(testBooks);
+  const bookRepository = createBookRepository(books);
+  const bookService = createBookService(bookRepository);
+  const resolvers = createBookResolvers(bookService);
+
+  return new ApolloServer({
+    typeDefs,
+    resolvers,
+  });
+};
