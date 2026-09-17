82c9f1cbb3af75ecbae799c6f5778a1d44d2e8fe
mohsinking2002 <mohsinraja4667@gmail.com>
2026-09-17 08:56:37 +0530

    add more books

--- server/src/data/data.js
+++ server/src/data/data.js
@@ -17,4 +17,46 @@ export const books = [
     author: "Martin Kleppmann",
     publishedYear: 2017,
   },
+  {
+    id: "4",
+    title: "Refactoring",
+    author: "Martin Fowler",
+    publishedYear: 1999,
+  },
+  {
+    id: "5",
+    title: "Domain-Driven Design",
+    author: "Eric Evans",
+    publishedYear: 2003,
+  },
+  {
+    id: "6",
+    title: "You Don't Know JS",
+    author: "Kyle Simpson",
+    publishedYear: 2015,
+  },
+  {
+    id: "7",
+    title: "The Mythical Man-Month",
+    author: "Frederick P. Brooks Jr.",
+    publishedYear: 1975,
+  },
+  {
+    id: "8",
+    title: "Don't Make Me Think",
+    author: "Steve Krug",
+    publishedYear: 2000,
+  },
+  {
+    id: "9",
+    title: "The DevOps Handbook",
+    author: "Gene Kim",
+    publishedYear: 2016,
+  },
+  {
+    id: "10",
+    title: "Accelerate",
+    author: "Nicole Forsgren",
+    publishedYear: 2018,
+  },
 ];
